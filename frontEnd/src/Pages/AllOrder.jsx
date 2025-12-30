a03077f0edafc942ae71f4cbcec28c79dec4efc4
rehmana42 <rehmana42537@gmail.com>
2025-12-30 19:05:30 +0530

    Fix conditional check for shop length

--- frontEnd/src/Pages/AllOrder.jsx
+++ frontEnd/src/Pages/AllOrder.jsx
@@ -25,7 +25,7 @@ const AllOrder = () => {
     </div>
         
       
-     : shop.length<=0 ?
+      !shop || shop?.length<=0 ?
       <div className="  mt-10 flex flex-col items-center justify-center gap-6 px-4 py-10 text-center">
 
 {/* Video */}
